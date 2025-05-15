d8a3f8b1800a6ba9fb298123b056345ddb74c82f
Jaxxen <jc1kp@proton.me>
2025-05-15 01:14:50 -0500

    Update foo.sh

--- foo.sh
+++ foo.sh
@@ -18,7 +18,7 @@ makepkg -si
 
 cd
 
-packages="devtools discord blender btop debugedit eog fastfetch foot galculator gimp gnome-keyring gnome-themes-extra gtk-engine-murrine hyprland hyprlock hyprpaper hyprsunset libreoffice-fresh ly mako mpv nwg-look obs-studio pavucontrol pcmanfm-gtk3 pipewire-pulse prismlauncher qbittorrent rnote signal-desktop starship ttf-cascadia-code-nerd ttf-font-awesome ufw waybar wev wine-gecko wine-mono wofi xdg-desktop-portal-hyprland yazi alarm-clock-applet bibata-cursor-theme-bin chromium-snapshot-bin github-desktop-bin gruvbox-dark-icons-gtk hyprshot librewolf-bin theclicker"
+packages="discord blender btop eog fastfetch foot galculator gimp gnome-keyring gnome-themes-extra gtk-engine-murrine hyprland hyprlock hyprpaper hyprsunset libreoffice-fresh ly mako mpv nwg-look obs-studio pavucontrol pcmanfm-gtk3 pipewire-pulse prismlauncher qbittorrent rnote signal-desktop starship ttf-cascadia-code-nerd ttf-font-awesome ufw waybar wev wine-gecko wine-mono wofi xdg-desktop-portal-hyprland yazi alarm-clock-applet bibata-cursor-theme-bin chromium-snapshot-bin github-desktop-bin gruvbox-dark-icons-gtk hyprshot librewolf-bin theclicker"
 
 for package in $packages; do
   paru -S --noconfirm "$package"
@@ -31,8 +31,6 @@ paru -S amd-ucode
 paru -S nvidia
 
 paru -S nvidia-settings
-
-paru -S ollama-cuda
 
 xdg-settings set default-web-browser librewolf.desktop
 
