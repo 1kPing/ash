09261ef5fa6f5936191f9984ba71925aac9dd1c3
Jaxxen <jc1kp@proton.me>
2025-05-29 03:11:46 -0500

    Update a.sh

--- a.sh
+++ a.sh
@@ -8,7 +8,9 @@ cd
 
 sudo mv pacman.conf /etc
 
-sudo pacman -Syu git
+sudo pacman -Syu --needed base-devel git rustup --noconfirm
+
+rustup default stable
 
 git clone https://aur.archlinux.org/paru.git
 
@@ -18,13 +20,11 @@ makepkg -si --noconfirm
 
 cd
 
-packages="discord btop eog fastfetch foot galculator gimp gnome-keyring gnome-themes-extra gtk-engine-murrine hyprland hyprlock hyprpaper hyprsunset libreoffice-fresh ly mako mpv nvim nwg-look obs-studio pavucontrol pcmanfm-gtk3 pipewire-pulse prismlauncher qbittorrent rnote rustup signal-desktop starship ttf-cascadia-code-nerd ttf-font-awesome ufw waybar wev wine-gecko wine-mono wofi xdg-desktop-portal-hyprland yazi alarm-clock-applet bibata-cursor-theme-bin chromium-snapshot-bin github-desktop-bin gruvbox-dark-icons-gtk hyprshot librewolf-bin theclicker"
+packages="discord btop eog fastfetch foot galculator gimp gnome-keyring gnome-themes-extra gtk-engine-murrine hyprland hyprlock hyprpaper hyprsunset libreoffice-fresh ly mako mpv nvim nwg-look obs-studio pavucontrol pcmanfm-gtk3 pipewire-pulse prismlauncher qbittorrent rnote signal-desktop starship ttf-cascadia-code-nerd ttf-font-awesome ufw waybar wev wine-gecko wine-mono wofi xdg-desktop-portal-hyprland yazi alarm-clock-applet bibata-cursor-theme-bin chromium-snapshot-bin github-desktop-bin gruvbox-dark-icons-gtk hyprshot librewolf-bin theclicker"
 
 for package in $packages; do
   paru -S --noconfirm "$package"
 done
-
-rustup default stable
 
 paru -S steam
 
@@ -36,6 +36,10 @@ paru -S nvidia nvidia-settings
 
 xdg-settings set default-web-browser librewolf.desktop
 
+sudo systemctl enable ly
+
+## gtk theme setup
+
 cd graphite-gtk-theme
 
 ./install.sh --tweaks rimless black
@@ -45,6 +49,10 @@ cd
 gsettings set org.gnome.desktop.interface gtk-theme 'Graphite-Dark'
 
 gsettings set org.gnome.desktop.interface icon-theme 'gruvbox-dark-icons-gtk'
+
+## eww installation
+
+git clone https://github.com/elkowar/eww
 
 cd eww
 
@@ -56,7 +64,7 @@ chmod +x ./eww
 
 cd
 
-sudo systemctl enable ly
+## cleaning up
 
 rm -rf .git
 
