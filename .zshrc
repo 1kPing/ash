fc0a336f64dd6b6cc85930b4344de447059bd03e
Jaxxen <jc1kp@proton.me>
2025-06-05 17:51:16 -0500

    update

--- .zshrc
+++ .zshrc
@@ -1,18 +1,21 @@
-eval "$(starship init zsh)"
+eval "$(starship init bash)"
 
 [[ -o interactive ]] || return
 
+#export XDG_SESSION_TYPE=x11
+#export SDL_VIDEODRIVER=x11
+#export QT_QPA_PLATFORM=xcb
+#export XCURSOR_THEME=Bibata-Original-Classic
+#export XCURSOR_SIZE=16
+#export LIBVA_DRIVER_NAME=nvidia
+#export __GLX_VENDOR_LIBRARY_NAME=nvidia
+#export __GL_GSYNC_ALLOWED=1
+#export __GL_VRR_ALLOWED=0
+
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
-alias os='ollama start'
-alias ds='ollama run deepseek-r1:14b'
-alias phi='ollama run phi4:14b'
-alias eww='~/eww/target/release/eww'
-alias fc='sudo theclicker run -d"/dev/input/event9" -l276 -r275 -c25 -C0 -H'
-alias sc='sudo theclicker run -d"/dev/input/event9" -l276 -r275 -c1000 -C0'
-alias ue='~/ue/Engine/Binaries/Linux/UnrealEditor'
-alias e='nvim'
-alias se='sudo nvim'
 alias ff='fastfetch'
 alias y='yazi'
 alias sy='sudo yazi'
+alias e='nvim'
+alias se='sudo nvim'
--- a.sh
+++ a.sh
@@ -1,81 +1,103 @@
 #!/bin/dash
 
-cd ~/ash
+trap 'exit 1' INT
 
-find . -maxdepth 1 -mindepth 1 -exec mv -f {} ~ \;
-
-cd
+echo "!! Everything currently in ~ will be moved to /home/old~N !!"
+sleep 1
+echo "Hit enter to proceed..."
+read answer
+if [ "$answer" = "^C" ]; then
+    exit 1
+else
+    echo "Backing up ~ to an available /home/old~N directory and moving configurations..."
+    
+    base_dir="/home/old~"
+    target_dir="$base_dir"
+    counter=2
+    
+    while [ -d "$target_dir" ]; do
+        target_dir="${base_dir}${counter}"
+        counter=$((counter + 1))
+    done
+    
+    sudo mkdir "$target_dir"
+    cd
+    sudo find . -maxdepth 1 -mindepth 1 -exec mv -f {} "$target_dir" \;
+    cd "$target_dir"/vsh
+    sudo find . -maxdepth 1 -mindepth 1 -exec mv -f {} ~ \;
+    cd
+    sudo rmdir "$target_dir"/vsh
+    sudo mkdir -p /usr/share/fonts/TTF
+    sudo mv -f ~/TTF/* /usr/share/fonts/TTF
+    rm -rf ~/.git
+    rmdir ~/TTF
+    rm ~/LICENSE
+    rm ~/README.md
+fi
 
 sudo mv pacman.conf /etc
-
 sudo pacman -Syu --needed base-devel git rustup --noconfirm
-
 rustup default stable
-
 git clone https://aur.archlinux.org/paru.git
-
 cd paru
-
 makepkg -si --noconfirm
-
 cd
 
-packages="discord btop eog fastfetch foot galculator gimp gnome-keyring gnome-themes-extra gtk-engine-murrine hyprland hyprlock hyprpaper hyprsunset libreoffice-fresh ly mako mpv nvim nwg-look obs-studio pavucontrol pcmanfm-gtk3 pipewire-pulse prismlauncher qbittorrent rnote signal-desktop starship ttf-cascadia-code-nerd ttf-font-awesome ufw waybar wev wine-gecko wine-mono wofi xdg-desktop-portal-hyprland yazi zsh alarm-clock-applet bibata-cursor-theme-bin chromium-snapshot-bin github-desktop-bin gruvbox-dark-icons-gtk hyprshot librewolf-bin theclicker"
+packages="hyprshot librewolf-bin pipewire-alsa blender btop fastfetch foot galculator gimp gnome-keyring gnome-themes-extra gtk-engine-murrine hyprland hyprlock hyprpaper hyprsunset imv libreoffice-fresh ly mako mpv nvim nwg-look pavucontrol pipewire-pulse prismlauncher qbittorrent sassc signal-desktop starship ttf-font-awesome ufw waybar wev wine-gecko wine-mono wofi xdg-desktop-portal-hyprland yazi zsh"
 
 for package in $packages; do
-  paru -S --noconfirm "$package"
+    paru -S --noconfirm "$package"
 done
 
-paru -S steam
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/kontr0x/github-desktop-install/main/installGitHubDesktop.sh)"
+sudo mv ~/binaries/* /usr/local/bin
+rmdir ~/binaries
+sudo mv ~/GitHubDesktop* /usr/local/bin/github
 
-paru -S blender
+echo "Do you want to install flatpak? (y/n)"
+read answer
+if [ "$answer" = "y" ]; then
+    sudo pacman -S flatpak --noconfirm
+    echo "Do you want to install discord through flatpak? (y/n)"
+        read answer
+	if [ "$answer" = "y" ]; then
+            sudo flatpak install com.discordapp.Discord -y
+        fi
+    echo "Do you want to install ungoogled chromium through flatpak? (y/n)"
+        read answer
+        if [ "$answer" = "y" ]; then
+            flatpak install io.github.ungoogled_software.ungoogled_chromium -y
+        fi
+fi
 
-paru -S amd-ucode
-
-paru -S nvidia nvidia-settings
+sudo pacman -S steam
+sudo pacman -S amd-ucode
+sudo pacman -S nvidia
 
 xdg-settings set default-web-browser librewolf.desktop
+xdg-mime default imv.desktop image/*
+xdg-mime default mpv.desktop video/*
+
+sudo ~/graphite-gtk-theme/other/grub2/install.sh -b
+~/graphite-gtk-theme/install.sh --tweaks rimless black
+gsettings set org.gnome.desktop.interface gtk-theme 'Graphite-Dark'
+gsettings set org.gnome.desktop.interface icon-theme 'gruvbox-dark-icons-gtk'
+sudo echo "QT_QPA_PLATFORMTHEME=gtk3" | sudo tee -a /etc/environment
+
+sudo mkdir -p /etc/alsa/conf.d
+sudo ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
+sudo ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
+echo "autospawn = no" | sudo tee /etc/pulse/client.conf
+
+rm -r ~/graphite-gtk-theme
+
+echo "arch linux + ly + hyprland: setup successful"
+
+echo "finished, reboot your computer"
 
 sudo systemctl enable ly
 
-## gtk theme setup
+rm ~/a.sh
 
-cd graphite-gtk-theme
+exit 0
 
-./install.sh --tweaks rimless black
-
-cd
-
-gsettings set org.gnome.desktop.interface gtk-theme 'Graphite-Dark'
-
-gsettings set org.gnome.desktop.interface icon-theme 'gruvbox-dark-icons-gtk'
-
-## eww installation
-
-git clone https://github.com/elkowar/eww
-
-cd eww
-
-cargo build --release --no-default-features --features=wayland
-
-cd target/release
-
-chmod +x ./eww
-
-cd
-
-## cleaning up
-
-rm -rf .git
-
-rm -r ash
-
-rm -r graphite-gtk-theme
-
-rm LICENSE
-
-rm README.md
-
-rm a.sh
-
-echo finished, reboot your computer
